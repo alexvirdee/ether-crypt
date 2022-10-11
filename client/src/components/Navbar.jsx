d6aa3e5d8142ff591dc39d6cb1976f6e64608c5a
Alexinder Virdee <alex.virdee08@gmail.com>
2022-10-10 21:06:48 -0400

    Setting up navigation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,29 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({ title, classProps }) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
+            {title}
+        </li>
+    )
+}
+
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+      <nav className="w-full flex md:justify-center justify-between items-center p-4">
+        <div className="md:flex-[0.5] flex-initial justify-center items-center">
+            <img src={logo} alt={logo} className="w-32 cursor-pointer" />
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+               {["market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                <NavbarItem key={item + index} title={item} />
+               ))}
+            </ul>
+            <li className="bg-[#2952e3] text-white py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">Login</li>
+        </div>
+      </nav>
     )
 }
 
